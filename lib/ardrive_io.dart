abd6e86f7b2fdce89e6b10f071ea9fc0e1dbacf8
Thiago Carvalho <thiagocarvalho.dev@gmail.com>
2023-08-23 09:38:08 -0300

    feat(save files on app dir)
    - add a method to save files on local app dir - ps: only mobile!

--- lib/ardrive_io.dart
+++ lib/ardrive_io.dart
@@ -8,6 +8,7 @@ export 'src/io_entity.dart';
 export 'src/io_exception.dart';
 export 'src/io_file.dart';
 export 'src/io_folder.dart';
+export 'src/mobile/mobile_io.dart';
 export 'src/utils/mime_type_utils.dart';
 export 'src/utils/path_utils.dart';
 export 'src/utils/permissions.dart';
